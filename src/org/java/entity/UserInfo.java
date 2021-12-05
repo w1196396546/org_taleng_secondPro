409a1420aef13eb468c5a77d79f44c4b9fb753fe
w1196396546 <1196396546@qq.com>
2021-12-05 11:16:11 +0800

    Commit Message

---
+++ src/org/java/entity/UserInfo.java
@@ -0,0 +1,36 @@
+package org.java.entity;
+
+import java.io.Serializable;
+
+public class UserInfo  implements Serializable {
+
+    private String userId;
+
+    private String userEmail;
+
+    private String userPwd;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+}
